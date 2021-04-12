6731d61e243a6c6ca7592bcd1a8174bac92bd17e
fuji <is0473si@ed.ritsumei.ac.jp>
2021-04-12 16:15:23 +0900

    no message

--- AutoCarProject/Assets/Scripts/CameraMove.cs
+++ AutoCarProject/Assets/Scripts/CameraMove.cs
@@ -15,24 +15,43 @@ public class CameraMove : MonoBehaviour
     public GameObject target3;
     public GameObject target4;
     private Tween cameraMove = null;
+    int pauseCount =0;
 
     void Start()
     {
 
-
+        Sequence sequence = DOTween.Sequence();
 
         // 3•b‚©‚¯‚Ä(5,0,0)‚ÖˆÚ“®‚·‚é
         //this.transform.DOMove(new Vector3(5f, 0f, 0f), 3f);
+        sequence.Append(
+        cameraMove = this.transform.DOPath(new Vector3[] { target1.transform.position, target1_5.transform.position, target2.transform.position, target3.transform.position, target4.transform.position, target1.transform.position }, 120f, PathType.Linear).SetLookAt(0.02f)) ;
+        sequence.SetLoops(-1,LoopType.Restart);
+
+        //sequence.stop();
         
-        var cameraMove = transform.DOPath(new Vector3[] { target1.transform.position, target1_5.transform.position, target2.transform.position, target3.transform.position, target4.transform.position, target1.transform.position }, 40f, PathType.Linear);
-        //cameraMove.SetLoops(-1,LoopType.Yoyo);
-        cameraMove.SetLookAt(0.03f);
+
+        
+        
     }
 
-    private void Update()
+    public void Pause()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (pauseCount % 2 == 0)
+            {
+                cameraMove.Pause();
 
+            }
+            else
+            {
+                cameraMove.Play();
+            }
+            pauseCount++;
+            Debug.Log("a");
+
+        }
 
     }
 }
