06983747a525cae91d11839b4d847b8ff8d4fd21
fuji <is0473si@ed.ritsumei.ac.jp>
2021-04-21 15:16:11 +0900

    キャラと道の追加

--- AutoCarProject/Assets/Scripts/CameraMove.cs
+++ AutoCarProject/Assets/Scripts/CameraMove.cs
@@ -14,6 +14,11 @@ public class CameraMove : MonoBehaviour
     public GameObject target2;
     public GameObject target3;
     public GameObject target4;
+    public GameObject target4_5;
+    public GameObject target5;
+    public GameObject target6;
+   
+  
     private Tween cameraMove = null;
     int pauseCount =0;
 
@@ -25,7 +30,8 @@ public class CameraMove : MonoBehaviour
         // 3•b‚©‚¯‚Ä(5,0,0)‚ÖˆÚ“®‚·‚é
         //this.transform.DOMove(new Vector3(5f, 0f, 0f), 3f);
         sequence.Append(
-        cameraMove = this.transform.DOPath(new Vector3[] { target1.transform.position, target1_5.transform.position, target2.transform.position, target3.transform.position, target4.transform.position, target1.transform.position }, 120f, PathType.Linear).SetLookAt(0.02f)) ;
+        cameraMove = this.transform.DOPath(new Vector3[] { target1.transform.position, target1_5.transform.position, target2.transform.position, target3.transform.position, target4.transform.position, target4_5.transform.position, target1.transform.position, target1.transform.position, target1_5.transform.position, target2.transform.position, target5.transform.position,target6.transform.position,target1.transform.position}, 500f, PathType.Linear).SetLookAt(0.02f)).SetEase(Ease.Linear) ;
+        
         sequence.SetLoops(-1,LoopType.Restart);
 
         //sequence.stop();
